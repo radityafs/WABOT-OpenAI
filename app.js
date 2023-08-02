063ccc427c218ef1fe7a27ec6c4062bd08094e26
Raditya <radit@student.uns.ac.id>
2023-08-02 23:09:11 +0700

    Update library to whiskeysockets/baileys

--- app.js
+++ app.js
@@ -1,68 +1,97 @@
-const makeWASocket = require('@adiwajshing/baileys').default
-const { BufferJSON, useMultiFileAuthState, DisconnectReason } = require('@adiwajshing/baileys');
-const fs = require('fs');
-const { Configuration, OpenAIApi } = require("openai")
+import { makeWASocket } from "@whiskeysockets/baileys";
+
+import {
+  useMultiFileAuthState,
+  DisconnectReason,
+} from "@whiskeysockets/baileys";
+import { Configuration, OpenAIApi } from "openai";
+import chalk from "chalk";
 
 const configuration = new Configuration({
-    apiKey: "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",
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
-
 async function connectToWhatsApp() {
-    const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys')
-    const sock = makeWASocket({
-        auth: state,
-        printQRInTerminal: true,
-        logger: 'off'
-    })
-    // this will be called as soon as the credentials are updated
-    sock.ev.on('creds.update', saveCreds)
+  const { state, saveCreds } = await useMultiFileAuthState("auth_info_baileys");
+  const sock = makeWASocket({
+    auth: state,
+    printQRInTerminal: true,
+  });
+  // this will be called as soon as the credentials are updated
+  sock.ev.on("creds.update", saveCreds);
 
-    sock.ev.on('connection.update', (update) => {
-        const { connection, lastDisconnect } = update
-        if (connection === 'close') {
-            if (typeof lastDisconnect !== 'undefined' && lastDisconnect !== null) {
-                const shouldReconnect = typeof lastDisconnect !== 'undefined' && typeof lastDisconnect.error !== 'undefined' && typeof lastDisconnect.error.output !== 'undefined' && typeof lastDisconnect.error.output.statusCode !== 'undefined' && lastDisconnect.error.output.statusCode != DisconnectReason.loggedOut
-                if (shouldReconnect) {
-                    connectToWhatsApp()
-                }
-            }
-        } else if (connection === 'open') {
-            console.log('opened connection')
+  sock.ev.on("connection.update", (update) => {
+    const { connection, lastDisconnect } = update;
+    if (connection === "close") {
+      if (typeof lastDisconnect !== "undefined" && lastDisconnect !== null) {
+        const shouldReconnect =
+          typeof lastDisconnect !== "undefined" &&
+          typeof lastDisconnect.error !== "undefined" &&
+          typeof lastDisconnect.error.output !== "undefined" &&
+          typeof lastDisconnect.error.output.statusCode !== "undefined" &&
+          lastDisconnect.error.output.statusCode != DisconnectReason.loggedOut;
+        if (shouldReconnect) {
+          connectToWhatsApp();
         }
-    })
-    sock.ev.on('messages.upsert', async ({ messages }) => {
+      }
+    } else if (connection === "open") {
+      console.log("opened connection");
+    }
+  });
+  sock.ev.on("messages.upsert", async ({ messages }) => {
+    const m = messages[0];
 
-        const m = messages[0]
+    if (!m.message) return; // if there is no text or media message
+    const messageType = Object.keys(m.message)[0]; // get what type of message it is -- text, image, video
 
-        if (!m.message) return // if there is no text or media message
-        const messageType = Object.keys(m.message)[0] // get what type of message it is -- text, image, video
-        if (messageType === 'conversation') {
-            if (typeof m.key.remoteJid !== 'undefined' && m.key.remoteJid !== null) {
-                const senderId = m.key.remoteJid
-                const senderMessage = m.message[messageType]
-                const response = await openai.createCompletion({
-                    model: "text-davinci-003",
-                    prompt: senderMessage,
-                    temperature: 0,
-                    max_tokens: 1000,
-                    top_p: 1,
-                    frequency_penalty: 0.2,
-                    presence_penalty: 0,
-                });
+    if (
+      messageType === "conversation" ||
+      messageType === "extendedTextMessage"
+    ) {
+      if (typeof m.key.remoteJid !== "undefined" && m.key.remoteJid !== null) {
+        try {
+          const senderId = m.key.remoteJid;
+          const senderMessage =
+            m.message.conversation || m.message.extendedTextMessage.text;
 
-                const resultnya = response.data.choices[0].text;
+          const command = senderMessage?.toLowerCase()?.substring(0, 4);
+          const message = senderMessage?.toLowerCase()?.substring(5);
 
-                await sock.sendMessage(senderId, { text: resultnya })
-                console.log(`Message from ${senderId}: ${senderMessage} | Response: ${resultnya}`)
-            }
-        } else {
-            // console.log(m)
+          if (command === "!ask") {
+            const completion = await openai.createCompletion({
+              model: "text-davinci-003",
+              prompt: message,
+              temperature: 0.5,
+              max_tokens: 1024,
+              top_p: 0.3,
+              frequency_penalty: 0.5,
+              presence_penalty: 0.0,
+            });
+
+            const responseMessage = completion.data.choices[0].text;
+            await sock.sendMessage(senderId, { text: responseMessage.trim() });
+
+            console.log(
+              chalk.green(
+                `[!ask] ${senderId} asked ${message} and got ${responseMessage} as a response.`
+              )
+            );
+          } else {
+            if (m.key.fromMe) return;
+
+            console.log(
+              chalk.red(
+                `[chat] ${senderId} sent ${senderMessage} [ ${messageType}]`
+              )
+            );
+          }
+        } catch (error) {
+          console.log(`Got an error: ${error}`);
         }
-    })
-
-
+      }
+    }
+  });
 }
 // run in main file
-connectToWhatsApp()
+connectToWhatsApp();
